f4fd5a75e88927f51557a0fd6c61128b5222d08c
xsluo <isoluo@gmail.com>
2015-03-16 14:14:49 +0800

    f d sa s d first

---
+++ eHealth/ScheduleTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  ScheduleTableViewController.h
+//  eHealth
+//
+//  Created by Bagu on 15/3/15.
+//  Copyright (c) 2015年 PanGu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class Doctor;
+@interface ScheduleTableViewController : UITableViewController
+@property(nonatomic,retain) Doctor *doctor;
+@property (strong,nonatomic) NSMutableArray *scheduleList;
+@end
