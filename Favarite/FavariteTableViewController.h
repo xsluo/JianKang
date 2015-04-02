c71b4fd1da404b1d4f3a737abe163a624c10c1cd
xsluo <isoluo@gmail.com>
2015-04-02 14:47:47 +0800

    2015/4/2
    test on service

---
+++ Favarite/FavariteTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  FavariteTableViewController.h
+//  eHealth
+//
+//  Created by Bagu on 15/4/2.
+//  Copyright (c) 2015年 PanGu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavariteTableViewController : UITableViewController
+- (IBAction)segmentTapped:(id)sender;
+
+@end
--- eHealth/DigitalHostpital/Consult/GetConsultViewController.m
+++ eHealth/DigitalHostpital/Consult/GetConsultViewController.m
@@ -8,7 +8,15 @@
 
 #import "GetConsultViewController.h"
 
+#define URL @"http://202.103.160.154:1210/WebAPI.ashx"
+#define Method @"GetConsult"
+#define AppKey @"JianKangEYuanIOS"
+#define AppSecret @"8D994823EBD9F13F34892BB192AB9D85"
+
 @interface GetConsultViewController ()
+@property(nonatomic,retain)  NSMutableData *responseData;
+@property(nonatomic,retain) NSArray *consultList;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -17,12 +25,107 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self setRequest];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void)setRequest{
+    
+    NSMutableDictionary *dictionary=[[NSMutableDictionary alloc] initWithCapacity:4];
+    [dictionary setObject:AppKey forKey:@"AppKey"];
+    [dictionary setObject:AppSecret forKey:@"AppSecret"];
+    [dictionary setObject:@"0" forKey:@"Type"];
+    NSError *error=nil;
+    
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictionary options:NSJSONWritingPrettyPrinted error:&error];
+    if(error){
+        NSLog(@"error:%@",error);
+    }
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    NSString *postString = [NSString stringWithFormat:@"method=%@&jsonBody=%@",Method,jsonString];
+    
+    NSData *postData = [postString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    //    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:URL] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:30];
+    //    [request setURL:[NSURL URLWithString:URL]];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:postData];
+    
+    //------------------------------
+        NSURLCache *urlCache = [NSURLCache sharedURLCache];
+        /* 设置缓存的大小为1M*/
+        [urlCache setMemoryCapacity:1*1024*1024];
+        NSCachedURLResponse *response = [urlCache cachedResponseForRequest:request];
+        //判断是否有缓存
+        if (response != nil){
+            NSLog(@"如果有缓存输出，从缓存中获取数据");
+            [request setCachePolicy:NSURLRequestReturnCacheDataDontLoad];
+        }
+    //-----------------------------
+    
+    NSURLConnection *connection = [[NSURLConnection alloc]initWithRequest:request delegate:self];
+    [connection start];
+}
+
+
+#pragma mark NSURLConnection Delegate Methods
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    // A response has been received, this is where we initialize the instance var you created
+    // so that we can append data to it in the didReceiveData method
+    // Furthermore, this method is called each time there is a redirect so reinitializing it
+    // also serves to clear it
+    _responseData = [[NSMutableData alloc] init];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    // Append the new data to the instance variable you declared
+    [_responseData appendData:data];
+}
+
+- (NSCachedURLResponse *)connection:(NSURLConnection *)connection
+                  willCacheResponse:(NSCachedURLResponse*)cachedResponse {
+    // Return nil to indicate not necessary to store a cached response for this connection
+    //    return nil;
+    return cachedResponse;
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    // The request is complete and data has been received
+    // You can parse the stuff in your instance variable now
+    
+    if([self.responseData length]==0)
+        return;
+    NSError *error = nil;
+    NSDictionary *jsonDictionary = [NSJSONSerialization JSONObjectWithData:self.responseData  options:NSJSONReadingMutableContainers  error:&error];
+    if (jsonDictionary == nil) {
+        NSLog(@"json parse failed \r\n");
+        return;
+    }
+    self.consultList =[jsonDictionary objectForKey:@"ConsultList"];
+    [self.tableView reloadData];
+    
+    UIApplication* app = [ UIApplication  sharedApplication ];
+    app.networkActivityIndicatorVisible = NO;
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    // The request has failed for some reason!
+    // Check the error var
+    NSLog(@"%@",[error localizedDescription]);
+    
+    UIApplication* app = [ UIApplication  sharedApplication ];
+    app.networkActivityIndicatorVisible = NO;
+//    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:nil message:@"网络无法连接" delegate:self cancelButtonTitle:@"确定"otherButtonTitles:nil, nil];
+//    [alert show];
+    
+}
+
 
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     return 1;
@@ -39,8 +142,9 @@
     if (cell==nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reuseIndentifier];
     }
-//    NSInteger row = [indexPath row];
-    cell.textLabel.text = @"咨询列表";
+    NSInteger row = [indexPath row];
+    NSDictionary *consult = [self.consultList objectAtIndex:row];
+    cell.textLabel.text = [consult objectForKey:@"ConsultName"];
     return cell;
 }
 /*
